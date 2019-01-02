629c2f29c4aaddcd9ff1414e58ee807dcfec08c5
shunny <shunmok@shunny.kr>
2019-01-01 21:46:22 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# jenkins-pipeline
+
+## Description 
+
+A collection of Jenkins pipelines for various languages. The pipelines are very barebones and basic but extensible and completely customizable.
+
+## Installation 
+If you will like to use this project without any modification then follow the instructions here: 
+* https://jenkins.io/doc/book/pipeline/shared-libraries/#global-shared-libraries
+
+However, I highly recommend you create a fork and customize the pipeline according to your needs and requirements. Using your forked version follows the same steps as above. 
+
+## License 
+MIT License
---
+++ vars/CplusplusPipeline.groovy
@@ -0,0 +1,63 @@
+def call(body) {
+    // Get paramters defined in Jenkinsfile if any
+    Map config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    // Set properties to variables, so you dont need to call config on every call
+    def nodeLabel = config.nodeLabel
+    def buildTimeout = config.buildTimeout != null ? config.buildTimeout : 15
+    def projectName = config.projectName
+
+    pipeline {
+        timestamps {
+            // Set build agent if defined
+            nodeLabel != null ? agent { label "${nodeLabel}" } : agent any
+
+            def workspace = pwd()
+            def gitBranch = env.BRANCH_NAME
+            def buildNumber = env.BUILD_NUMBER
+            def packageName = "${projectName}-${gitBranch}-${buildNumber}.zip"
+
+            stages {
+                stage("Checkout ${projectName} from SCM") {
+                    checkout scm
+                }
+
+                stage('Building project') {
+                    timeout(time: buildTimeout, unit: 'MINUTES') {
+                        steps {
+                            sh 'mkdir build'
+                            dir('build') {
+                                sh 'cmake ..'
+                                sh 'make'
+                            }
+                            zip(zipFile: "${packageName}", dir:"${workspace}/build/${projectName}")
+                        }
+                    }
+                }
+
+                stage('Running tests') {
+
+                }
+
+                stage('Publishing to artifactory') {
+                    def packageZip = "${workspace}/${packageName}"
+                    def artifactoryUploadSpec =
+                        """
+                            {
+                              "files": [
+                                {
+                                  "pattern": "${executable}",
+                                  "target": "generic-local"
+                                }
+                              ]
+                            }
+                        """
+                    def uploadToArtifactory = artifactoryServer.upload spec: artifactoryUploadSpec
+                }
+            }
+        }
+    }
+}
